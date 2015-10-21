c09f4dbe96c60c423fd1c9dd0cc1d4fab451afae
Manuel Nakamurakare <manask88@gmail.com>
2015-10-21 13:45:10 -0700

    added elapsed time

--- index.ios.js
+++ index.ios.js
@@ -12,58 +12,99 @@ var {
   Text,
   View,
   SliderIOS,
-  Component
+  Component,
+  ActivityIndicatorIOS
 } = React;
 
 class videoplayerTest extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: 0}
+    this.state = {
+      value: 0,
+      isLoading: true,
+      rate: 0.5
+    };
   }
 
-  setDuration(value){
-    this.setState({
-      duration: value.duration
-    })
+  _seekTo(value){
+    console.log(value+"ddd")
+    this.setState({value: value});
+    this.refs.video.seek(value);
   }
-  setTime(value){
-    console.log('setTime'+JSON.stringify(value))
+
+  _onLoad(value){
+    this.setState({
+      duration: value.duration,
+      isLoading: false,
+    });
+  }
+  _onProgress(value){
+ console.log('setTime'+JSON.stringify(value))
     this.setState({
       currentTime: value.currentTime
-    })
+    });
   }
 
+  _replay(){
+    this.setState({
+      currentTime: 0
+    });
+  }
+
+
+  _onEnd(){
+
+  }
+
+  _onVideoError(){
+    console.log('_onError')
+  }
   render () {
+
+    var activityIndicatorIOS = this.state.isLoading ? <ActivityIndicatorIOS size="large" style={styles.activityIndicatorIOS} /> : false;
     return (
       <View style={styles.container}>
+
         <Video
+          style={styles.backgroundVideo}
+
           ref='video'
           source={{uri: "http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4"}}
           style={styles.backgroundVideo}
-          rate={1}
+          rate={this.state.rate}
           volume={1}
-          muted={true}
-          onLoad={this.setDuration.bind(this)}
-          onProgress={this.setTime.bind(this)}
+          muted={false}
+          onLoad={this._onLoad.bind(this)}
+          onProgress={this._onProgress.bind(this)}
+          onEnd={this._onEnd.bind(this)}
+          onError={this._onVideoError.bind(this)}
           resizeMode="cover"
           repeat={false}
           key="video1" />
-
+        {activityIndicatorIOS}
+        <Text style={styles.elapsedTime}>
+          {this.state.currentTime}
+        </Text>
         <SliderIOS
           minimumValue={0}
           maximumValue={this.state.duration}
           style={styles.slider}
           value={this.state.currentTime}
-          onValueChange={(value) => {
-            this.setState({value: value})
-            this.refs.video.seek(value)
-      } } />
+          onValueChange={this._seekTo.bind(this)} />
       </View>
     );
   }
 };
 
 var styles = StyleSheet.create({
+  activityIndicatorIOS: {
+    backgroundColor: 'transparent',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: 100,
+    height: 100,
+  },
   backgroundVideo: {
     position: 'absolute',
     top: 0,
@@ -77,7 +118,9 @@ var styles = StyleSheet.create({
   //  alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-
+  elapsedTime: {
+    alignSelf: 'flex-start',
+  },
   slider: {
    alignSelf: 'center',
     height: 10,
